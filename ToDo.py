29b4e10a6407f9d4ac8755bf80f0d279910a21d4
Leeshin14 <tlsgur0214@gmail.com>
2024-09-25 03:28:56 +0900

    ToDo.py

--- ToDo.py
+++ ToDo.py
@@ -22,7 +22,7 @@ def fetch_and_delete_done_tasks():
     now = datetime.now()
 
     # 원하는 시간 범위 체크 (예: 오전 2시 33분에만 실행)
-    if (17 <= now.hour < 18):
+    if (3 <= now.hour < 4):
         print("Task executed at:", now)
 
         # 여기에서 실제 작업 로직을 수행합니다.
